ddf40a220df1b60ace6295e93d7e0378765e3497
liujia <953993047@qq.com>
2020-01-03 01:08:26 +0800

    fea(login.vue): 添加校验

--- client/src/pages/Login.vue
+++ client/src/pages/Login.vue
@@ -4,10 +4,7 @@
       <el-tabs v-model="activeName">
         <el-tab-pane label="注册" name="first">
             <el-form  label-width="80px" :model="form" :rules="rules" ref="form">
-              <el-form-item
-                label="用户名"
-                prop="account"
-              >
+              <el-form-item label="用户名" prop="account">
                 <el-input v-model="form.account"></el-input>
               </el-form-item>
               <el-form-item label="密码" prop="password">
@@ -28,18 +25,14 @@
 
         </el-tab-pane>
         <el-tab-pane label="登录" name="second">
-          <el-form  label-width="80px" :model="user" :rule=rules>
-            <el-form-item label="用户名">
-              <el-input v-model="user.account" :rules="[
-                  { required: true, message: '用户名不能为空'}
-                ]"></el-input>
+          <el-form  label-width="80px" :model="user" :rules="rules" ref="user">
+            <el-form-item label="用户名" prop="account">
+              <el-input v-model="user.account"></el-input>
             </el-form-item>
-            <el-form-item label="密码">
-              <el-input v-model="user.password" :rules="[
-                  { required: true, message: '密码不能为空'}
-                ]" show-password></el-input>
+            <el-form-item label="密码" prop="password">
+              <el-input v-model="user.password" show-password></el-input>
             </el-form-item>
-            <el-form-item label="验证码" ref="liujia">
+            <el-form-item label="验证码" prop="captcha">
                 <el-col :span="16">
                   <el-input type="text" v-model="user.captcha"></el-input>
                 </el-col>
@@ -51,7 +44,7 @@
               <!-- <Verify @success="verify=true" @error="verify=false" :type="1"></Verify> -->
             </el-form-item>
             <el-form-item>
-              <el-button type="primary" @click="login">登录</el-button>
+              <el-button type="primary" @click="login('user')">登录</el-button>
             </el-form-item>
         </el-form>
       </el-tab-pane>
@@ -69,6 +62,11 @@ export default {
     return {
       activeName:'second',
       form:{
+        account:"",
+        password:"",
+        captcha:""
+      },
+      user:{
         account:"",
         password:"",
         captcha:""
@@ -105,27 +103,26 @@ export default {
               }
             });
           }
-        });
+      });
     },
-    login(){
-      const form=this.user;
-      const that=this;
-      form.password=this.CalcuMD5(form.password);
-      form.captcha=form.captcha.toLowerCase();
-      // if(this.verify===false){
-      //    this.$message('验证码错误');
-      // }
-      this.$ajax.post('/api/login',form).then(function(res){
-        if(res.data.code==0){
-          // Cookies.set('token',res.data.data.token,{ expires: 9999 })
-          console.log(res.data.data.uid)
-          localStorage.setItem('uid',res.data.data.uid)
-          console.log(res.data.uid)
-          that.$router.push('/welcome')
-        }else{
-          that.$message(res.data.message)
-        }
-      })
+    login(formName){
+      this.$refs[formName].validate((valid) => {
+          if (valid) {
+            const form=this.user;
+            const that=this;
+            form.password=this.CalcuMD5(form.password);
+            form.captcha=form.captcha.toLowerCase();
+            this.$ajax.post('/api/login',form).then(function(res){
+              if(res.data.code==0){
+                localStorage.setItem('uid',res.data.data.uid)
+                that.$router.push('/welcome')
+              }else{
+                that.$message(res.data.message)
+              }
+            })
+          }
+      });
+
     },
     CalcuMD5(pwd) {
       pwd = pwd.toUpperCase();
